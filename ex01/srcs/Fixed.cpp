ed8da4a6a18cfd1286e3775d7d79365224ebe132
urabexon <hiroki708.a48uk@gmail.com>
2025-01-06 22:23:13 +0900

    fix ex00

--- ex01/srcs/Fixed.cpp
+++ ex01/srcs/Fixed.cpp
@@ -1,24 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hurabe <hurabe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/06 20:42:51 by hurabe            #+#    #+#             */
-/*   Updated: 2025/01/06 22:21:28 by hurabe           ###   ########.fr       */
+/*   Created: 2025/01/06 22:20:28 by hurabe            #+#    #+#             */
+/*   Updated: 2025/01/06 22:20:29 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Fixed.hpp"
-
-int main( void ) {
-Fixed a;
-Fixed b( a );
-Fixed c;
-c = b;
-std::cout << a.getRawBits() << std::endl;
-std::cout << b.getRawBits() << std::endl;
-std::cout << c.getRawBits() << std::endl;
-return 0;
-}
--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -5,20 +5,25 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hurabe <hurabe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/06 20:42:51 by hurabe            #+#    #+#             */
-/*   Updated: 2025/01/06 22:21:28 by hurabe           ###   ########.fr       */
+/*   Created: 2025/01/06 22:20:26 by hurabe            #+#    #+#             */
+/*   Updated: 2025/01/06 22:21:13 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Fixed.hpp"
-
+#include <iostream>
 int main( void ) {
 Fixed a;
-Fixed b( a );
-Fixed c;
-c = b;
-std::cout << a.getRawBits() << std::endl;
-std::cout << b.getRawBits() << std::endl;
-std::cout << c.getRawBits() << std::endl;
+Fixed const b( 10 );
+Fixed const c( 42.42f );
+Fixed const d( b );
+a = Fixed( 1234.4321f );
+std::cout << "a is " << a << std::endl;
+std::cout << "b is " << b << std::endl;
+std::cout << "c is " << c << std::endl;
+std::cout << "d is " << d << std::endl;
+std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 return 0;
 }
